d775e4cbebaefcc027e240e59516e7aba0e421b0
Quang Phan <oclockvn@gmail.com>
2024-01-04 11:57:05 +0700

    file handling

---
+++ FileHandling/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace FileHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "MOCK_DATA.csv";
+            var lines = File.ReadAllLines(path);
+            var result = new List<string>();
+
+            foreach (var line in lines.Skip(1))
+            {
+                var arr = line.Split(','); // [1,Carny,Salerg,1991-04-30]
+                if (!DateTime.TryParse(arr[3], out var date))
+                {
+                    continue;
+                }
+
+                var yearOld = DateTime.Now.Year - date.Year;
+                if (yearOld < 18)
+                {
+                    continue;
+                }
+
+                result.Add($"{arr[1]} {arr[2]} is {yearOld} years old");
+            }
+
+            if (result.Count == 0)
+            {
+                return;
+            }
+
+            // write to file
+            File.WriteAllLines($"output-{DateTime.Now:MMddhhmmss}.txt", result.ToArray());
+        }
+    }
+}
---
+++ Week17.Lib/AccountService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week17.Lib
+{
+    public struct Account
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public struct RegisterResult
+    {
+        public bool IsValid { get; set; }
+        public string? Error { get; set; }
+    }
+
+    public class AccountService
+    {
+        private List<Account> accounts = [];
+
+        public RegisterResult RegisterAccount(string username, string password)
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(username, "username");
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(password, "password");
+
+            var usernameExist = accounts.Any(a => a.Email == username); 
+            if (usernameExist)
+            {
+                return new RegisterResult { Error = "Username already exists" };
+            }
+
+            // check strong password
+            // ...
+
+            accounts.Add(new Account
+            {
+                Email = username,
+                Password = HashString(password), // todo: encrypt the password
+            });
+
+            return new RegisterResult { IsValid = true };
+        }
+
+        public bool ValidateAccount(string username, string password)
+        {
+            // if incorrect username
+            // -> username does not exist
+            // else if incorrect password
+            // -> password is incorrect
+            // username or password is incorrect
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(username, "username");    
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(password, "password");
+
+            var hashedPassword = HashString(password); // 2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824
+            var accountExist = accounts.Where(x => x.Email == username && x.Password == hashedPassword)
+                .Any();
+
+            return accountExist;
+        }
+
+        static string HashString(string input)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                // Convert the byte array to a hexadecimal string
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < hashedBytes.Length; i++)
+                {
+                    stringBuilder.Append(hashedBytes[i].ToString("x2"));
+                }
+
+                return stringBuilder.ToString();
+            }
+        }
+    }
+}
