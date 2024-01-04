4c6190e8c1ecb5fc778577102737231578dd1cbe
Quang Phan <oclockvn@gmail.com>
2024-01-04 16:32:21 +0700

    download file

---
+++ FileHandling/MoveFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandling
+{
+    public class MoveFile
+    {
+        public static string CopyFile(string source, string destination)
+        {
+            return null;
+        }
+    }
+}
