073bfca405e84cf8e65c3eb83410cbb67fd13eb5
Quang Phan <oclockvn@gmail.com>
2024-01-04 16:20:38 +0700

    read large file

--- FileHandling/Program.cs
+++ FileHandling/Program.cs
@@ -4,34 +4,9 @@
     {
         static void Main(string[] args)
         {
-            var path = "MOCK_DATA.csv";
-            var lines = File.ReadAllLines(path);
-            var result = new List<string>();
-
-            foreach (var line in lines.Skip(1))
-            {
-                var arr = line.Split(','); // [1,Carny,Salerg,1991-04-30]
-                if (!DateTime.TryParse(arr[3], out var date))
-                {
-                    continue;
-                }
-
-                var yearOld = DateTime.Now.Year - date.Year;
-                if (yearOld < 18)
-                {
-                    continue;
-                }
-
-                result.Add($"{arr[1]} {arr[2]} is {yearOld} years old");
-            }
-
-            if (result.Count == 0)
-            {
-                return;
-            }
-
-            // write to file
-            File.WriteAllLines($"output-{DateTime.Now:MMddhhmmss}.txt", result.ToArray());
+            var email = ReadingLargeFile.GetFirstEmailUsingStream("sample_data.csv", 90);
+            Console.WriteLine(email);
+            Console.ReadKey();
         }
     }
 }
---
+++ FileHandling/ReadingLargeFile.cs
@@ -0,0 +1,50 @@
+﻿namespace FileHandling
+{
+    public class ReadingLargeFile
+    {
+        /// <summary>
+        /// Read file from given path and return the email of first record that has balance <= maxValue
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="maxValue"></param>
+        /// <returns></returns>
+        public static string? GetFirstEmail(string path, int maxValue)
+        {
+            var lines = File.ReadAllLines(path);
+            foreach (var line in lines)
+            {
+                // 5,Dagny Chaudrelle,dchaudrelle4@cisco.com,94
+                var arr = line.Split(',');
+                var balance = int.Parse(arr.Last());
+                if (balance <= maxValue)
+                {
+                    return arr[2];
+                }
+            }
+
+            return null;
+        }
+
+        public static string? GetFirstEmailUsingStream(string path, int maxValue)
+        {
+            using var stream = new StreamReader(path);
+            while (!stream.EndOfStream)
+            {
+                var line = stream.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    continue;
+                }
+
+                var arr = line.Split(',');
+                var balance = int.Parse(arr.Last());
+                if (balance <= maxValue)
+                {
+                    return arr[2];
+                }
+            }
+
+            return null;
+        }
+    }
+}
