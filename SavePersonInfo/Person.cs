c1daf89eca60624cf57b93455029662f4e03cf01
Quang Phan <oclockvn@gmail.com>
2024-01-09 16:13:29 +0700

    parse file

---
+++ SavePersonInfo/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace SavePersonInfo
+{
+    public struct Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ SavePersonInfo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SavePersonInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            var people = PersonService.ReadFile("people.txt");
+            PersonService.WriteToFile(people, "output");
+
+            Console.WriteLine("File is saved successfully");
+            Console.ReadKey();
+        }
+    }
+}
